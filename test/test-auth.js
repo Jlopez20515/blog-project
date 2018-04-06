310c07784909e06b4e3dae79fae0ddb4811fea7f
Jesse Lopez <Jlopez20515@gmail.com>
2018-04-05 17:41:15 -0700

    modified the .env

--- test/test-auth.js
+++ test/test-auth.js
@@ -2,7 +2,7 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const { app, runServer, closeServer } = require('../server');
 const { User } = require('../users');
